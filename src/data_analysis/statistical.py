4e28596a1f033388aaca244189e65dbc10cd12fa
Alena-Kathrin Golla <alena-kathrin@hotmail.de>
2021-03-18 13:47:00 +0100

    added documentation for filter by variance, added euclidian distance

--- src/data_analysis/statistical.py
+++ src/data_analysis/statistical.py
@@ -2,6 +2,32 @@ import numpy as np
 variance_threshold = 1.0e-5
 
 
-def filter_by_variance(df, threshold=variance_threshold):
+def filter_by_variance_pd(df, threshold=variance_threshold):
+    """Filters entries in df by their variance. Entries with variance lower than threshold are dropped.
+
+    Args:
+        df (data frame): A list with the indices of the reference vectors.
+        threshold (float): Threshold value used for filtering.
+
+    Returns:
+        data frame: only contains entries with high variance."""
     variance = np.var(df.values, axis=0)
     return df.drop(columns=df.columns[(variance < threshold)])
+
+
+def euclidean_distance_pd(list_ref, list_comp, df):
+    """Calculates the Euclidean distance (L2 norm) between pairs of entries in df.
+
+    Args:
+        list_ref (integer list): A list with the indices of the reference vectors.
+        list_comp (integer list): A list with the indices of the vectors to\
+        compare to.
+        df (data frame): The data object.
+
+    Returns:
+        numpy array: The Euclidean distance (L2 norm) for comparison vs. reference\
+        vectors."""
+    distances = np.zeros(len(list_ref))
+    for i in range(len(list_ref)):
+        distances[i] = np.linalg.norm(df[list_comp[i]] - df[list_ref[i]])
+    return distances
