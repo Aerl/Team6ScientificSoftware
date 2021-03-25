fixed numerical

src/data_analysis/numerical.py
@@ -16,10 +16,7 @@ def autocorr_single_tp(a:np.array, t:int)->float:
     float
         The autocorrelation as a real number.
     """
-    if t == 0:
-         return np.real(np.sum(a * np.conj(a)))
-    else:
-        return np.real(np.sum(a[t:] * np.conj(a[:-t])))
+    return np.real(np.sum(a[0] * np.conj(a[t])))
 
 def autocorr(df:pd.DataFrame)->pd.DataFrame:
     """Do autocorrelation for all possible time steps over all columns.

src/data_analysis/tests/test_numerical.py
@@ -6,31 +6,30 @@ import numpy as np
 from data_analysis import numerical
 
 class build_wf:
-    def __init__(self, dim1, dim2, myval):
+    def __init__(self, dim1, dim2):
         self.dim1 = dim1
         self.dim2 = dim2
-        self.myval = myval
 
-    def init_array(self):
+    def init_array(self, myval):
         arr = np.ones((self.dim1, self.dim2), dtype=complex)
-        return arr*self.myval
+        return arr*myval
 
-    def init_vector(self):
+    def init_vector(self, myval):
         arr = np.ones((self.dim1,), dtype=complex)
-        return arr*self.myval
+        return arr*myval
 
 @pytest.fixture(scope='module')
 def init_obj():
-    obj = build_wf(3, 3, 1.0)
+    obj = build_wf(3, 3)
     return obj
 
 @pytest.fixture
 def test_wf(init_obj):
-    return init_obj.init_array()
+    return init_obj.init_array(1)
 
 @pytest.fixture
 def ref_auto(init_obj):
-    return init_obj.init_vector()
+    return init_obj.init_vector(3)
 
 def test_autocorr_single():
     test_array = np.random.rand(8)
@@ -39,5 +38,4 @@ def test_autocorr_single():
     pytest.approx(numerical.autocorr_single_tp(test_array, 0), 1)
     
 def test_autocorr(test_wf, ref_auto):
-    print(numerical.autocorr(pd.DataFrame(test_wf)).values)
     assert np.allclose(numerical.autocorr(pd.DataFrame(test_wf)).values, ref_auto)